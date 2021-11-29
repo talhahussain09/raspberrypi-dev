9bcb31e5893a2e04c2e9a2e9d9700ca809ee0923
talhahussain09 <49669460+talhahussain09@users.noreply.github.com>
2021-11-30 00:27:24 +0500

    Add files via upload

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,19 +1,34 @@
-# test_connect.py 
-import paho.mqtt.client as mqtt 
+#This is a working prototype. DO NOT USE IT IN LIVE PROJECTS
+import paho.mqtt.client as mqtt
+import time
+import ScanUtility
+import bluetooth._bluetooth as bluez
 
-# The callback function. It will be triggered when trying to connect to the MQTT broker
-# client is the client instance connected this time
-# userdata is users' information, usually empty. If it is needed, you can set it through user_data_set function.
-# flags save the dictionary of broker response flag.
-# rc is the response code.
-# Generally, we only need to pay attention to whether the response code is 0.
 def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected success")
-    else:
-        print(f"Connected fail with code {rc}")
+    print(f"Connected with result code {rc}")
 
-client = mqtt.Client() 
-client.on_connect = on_connect 
-client.connect("broker.emqx.io", 1883, 60) 
-client.loop_forever()
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect("broker.emqx.io", 1883, 60)
+#Set bluetooth device. Default 0.
+dev_id = 0
+try:
+	sock = bluez.hci_open_dev(dev_id)
+	print ("\n *** Looking for BLE Beacons ***\n")
+	print ("\n *** CTRL-C to Cancel ***\n")
+except:
+	print ("Error accessing bluetooth")
+
+ScanUtility.hci_enable_le_scan(sock)
+#Scans for iBeacons
+
+try:
+	while True:
+		returnedList = ScanUtility.parse_events(sock, 10)
+        client.publish('raspberry/topic', payload=returnedList, qos=0, retain=False)
+			
+		for item in returnedList:
+			print(item)
+			print("")
+except KeyboardInterrupt:
+    pass
